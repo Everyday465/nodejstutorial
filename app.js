c93746bbba2d89a10c541c8b4f63ca137b4f5c75
Everyday465 <110691604+Everyday465@users.noreply.github.com>
2025-04-28 21:36:08 +0800

    tutorial 3-6

--- app.js
+++ app.js
@@ -46,6 +46,7 @@
 
  */
 
+ /* tutorial 3 Readline
  const readline = require('readline');
  const rl = readline.createInterface({
     input : process.stdin,
@@ -65,6 +66,7 @@
         rl.setPrompt('Incorrect resposne, please try again. \n');
         rl.prompt();
         //listens to user input and check
+        //creates loop until
         rl.on('line',(userInput)=>{
             if(userInput.trim()==answer){
                 rl.close();
@@ -80,4 +82,117 @@
 rl.on('close',()=>{
     console.log("correct!!");
 })
- 
+
+*/
+
+
+/* tutorial 4 file system module part 1
+const fs = require('fs');
+//create file
+// fs.writeFile('example.txt',"this is an example",(err)=>{
+//     if(err){
+//         console.log(err);
+//     }
+//     else {
+//         console.log('file successfully created');
+//         //need to have encoding method
+//         fs.readFile('example.txt','utf8',(err,file)=>{
+//             if(err){
+//                 console.log(err);
+//             } else{
+//                 console.log(file);
+//             }
+//         })
+//     }
+// })
+
+// fs.rename('example.txt','example2.txt',(err)=>{
+//     if(err){
+//         console.log(err);
+//     } else{
+//         console.log("file successfully renamed!");
+//     }
+// })
+
+// fs.appendFile('example2.txt',"Some data being appended",(err)=>{
+//     if(err){
+//         console.log(err);
+//     } else{
+//         console.log("successfully appended data to file");
+//     }
+// })
+
+
+// fs.unlink("example2.txt",(err)=>{
+//     if(err){
+//         console.log(err);
+//     } else{
+//         console.log('successfuly deleted file');
+//     }
+// })
+*/
+
+
+/* tutorial 5 folder - file system module part 
+
+const fs = require('fs');
+// fs.mkdir('tutorial',(err)=>{
+//     if(err){
+//         console.log(err);
+//     } else {
+//         fs.writeFile('./tutorial/example.txt',"this is an example",(err)=>{
+//             if (err){
+//                 console.log(err);
+//             } else {
+//                 console.log("successfully created file.")
+//             }
+//         })
+//         console.log("successfully created folder");
+        
+//     }
+// })
+// fs.unlink('./tutorial/example.txt',(err=>{
+//     if (err){
+//         console.log(err);
+//     } else {
+//         fs.rmdir('tutorial', (err)=>{
+//             if(err){
+//                 console.log(err)
+//             } else {
+//                 console.log("folder successfully deleted")
+//             }
+//         })
+//     }
+// }))
+
+// fs.readdir('example',(err,files)=>{
+//     if(err){
+//         console.log(err);
+//     } else {
+//         // console.log(files); //[ 'a.txt', 'b.txt' ]
+//         for (let file of files){
+//             fs.unlink('./example/'+file,(err)=>{
+//                 if (err){
+//                     console.log(err);
+//                 } else {
+//                     console.log(`successfully deleted ${file}`);
+//                 }
+//             })
+//         }
+//     }
+// })
+
+*/
+
+/* tutorial 6 read and write streams
+const fs = require('fs');
+
+const readStream = fs.createReadStream('./example.txt','utf8'); //reads data in chunks so it doesnt have to wait until all is read
+const writeStream = fs.createWriteStream('example2.txt');
+//readstream inherts the event listener module 
+readStream.on('data',(chunk)=>{
+    console.log(chunk);
+    writeStream.write(chunk);
+})
+
+*/
